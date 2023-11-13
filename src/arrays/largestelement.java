8151e7373665f0d68003995c1d34ee814518fe0d
SnehaS-14 <sneha014csec@gmail.com>
2023-11-13 14:30:42 +0530

    update largestelement

--- src/arrays/largestelement.java
+++ src/arrays/largestelement.java
@@ -1,4 +1,21 @@
-package arrays;
 
+package arrays;
+import java.util.*;
 public class largestelement {
+  public static void main(String[] args) {
+
+
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int arr[] = new int[n];//3
+    System.out.println("enter the number");
+    for(int i=0;i<n;i++) {
+      arr[i]=sc.nextInt();
+
+    }
+    Arrays.sort(arr);
+    System.out.println(arr[arr.length-1]);
+  }
 }
+
+
