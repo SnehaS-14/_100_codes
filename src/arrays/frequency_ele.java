c0f25f8f34e495d871e738c8e5753d5b0a910827
SnehaS-14 <sneha014csec@gmail.com>
2023-11-20 22:51:02 +0530

    update frequency_ele

---
+++ src/arrays/frequency_ele.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class frequency_ele {
+    public static void main(String[] args) {
+        int arr[]={1,1,2,3,3,4};
+        int count=0;
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i+1; j < arr.length ; j++) {
+                if(arr[i]==arr[j]){
+                    count++;
+
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
+
